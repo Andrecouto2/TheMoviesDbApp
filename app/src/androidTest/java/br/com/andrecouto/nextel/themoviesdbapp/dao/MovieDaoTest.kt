0cf4a63fc1fe942966fd4aa7557a55cc1c5d187f
andrecouto <andrecouto@brq.com>
2017-11-13 19:11:09 -0200

    test movieDao

--- app/src/androidTest/java/br/com/andrecouto/nextel/themoviesdbapp/dao/MovieDaoTest.kt
+++ app/src/androidTest/java/br/com/andrecouto/nextel/themoviesdbapp/dao/MovieDaoTest.kt
@@ -2,13 +2,17 @@ package br.com.andrecouto.nextel.themoviesdbapp.dao
 
 import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import br.com.andrecouto.nextel.themoviesdbapp.data.dao.MovieDAO
 import br.com.andrecouto.nextel.themoviesdbapp.data.dao.MoviesDatabase
 import br.com.andrecouto.nextel.themoviesdbapp.data.model.Movie
+import br.com.andrecouto.nextel.themoviesdbapp.ui.activity.MainActivity
 import junit.framework.Assert.assertEquals
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.*
@@ -16,15 +20,16 @@ import java.util.*
 /**
  * Created by andrecouto on 13/11/2017.
  */
-
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class MovieDaoTest {
-
 
     lateinit var movieDao: MovieDAO
     lateinit var database: MoviesDatabase
 
-    constructor()
+    @get:Rule
+    val mActivityTestRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Before
     fun setup() {
         val context = InstrumentationRegistry.getTargetContext()
@@ -38,12 +43,16 @@ class MovieDaoTest {
     }
 
     @Test
+    @Throws(Exception::class)
     fun testInsertedAndRetrievedMoviesMatch() {
         val users = arrayListOf(Movie(1, 0, false, 0.0, "Titanic", 0.0, "", "", "", "", false, "", Date(), "", 0), Movie())
         movieDao.insert(users)
 
-        val allUsers = movieDao.getById(1)
-        assertEquals(users, allUsers)
+        movieDao.findAll().doOnSuccess {
+              t: List<Movie> ->
+            assertEquals(users, t)
+        }
+
     }
 
 }
